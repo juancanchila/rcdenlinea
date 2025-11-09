b5159d2e0beead14a9c0be5927b8793b4dd80d8b
Juan Lopez <juan.canchila@gmail.com>
2025-11-09 19:58:52 +0000

    update conn

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('epacartagena', 'root', '1q2w3e4r5tPD!!', {
+const sequelize = new Sequelize('epacartagena_escombros_1', 'epacartagena_bd_5', '02v1Yj@9j', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
