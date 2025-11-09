8a388f62ac32bebfb42304052c29ba228aaf2511
Juan Lopez <juan.canchila@gmail.com>
2025-11-09 20:29:53 +0000

    all routes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const swaggerJsDoc = require('swagger-jsdoc');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const generadorRoutes = require('./routes/generadorRoutes');
+const transportadorRoutes = require('./routes/transportadorRoutes');
+const receptorRoutes = require('./routes/receptorRoutes');
+const proyectoRoutes = require('./routes/proyectoRoutes');
+const vehiculoRoutes = require('./routes/vehiculoRoutes');
+const resolucionRoutes = require('./routes/resolucionRoutes');
+const visitatecnicaRoutes = require('./routes/visitaTecnicaRoutes');
+const reporteImpPmaRcdRoutes = require('./routes/reporteImpPmaRcdRoutes');
+const reporteReporteRcdGenerador = require('./routes/rcdgeneradorRoutes');
+const roleRoutes = require('./routes/roleRoutes');
+const pinRoutes = require('./routes/pinRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -41,6 +51,17 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/generador', generadorRoutes);
+
+app.use('/api/transportador', transportadorRoutes);
+app.use('/api/receptor', receptorRoutes);
+app.use('/api/proyecto', proyectoRoutes);
+app.use('/api/vehiculo', vehiculoRoutes);
+app.use('/api/resolucion', resolucionRoutes);
+app.use('/api/visitatecnica', visitatecnicaRoutes);
+app.use('/api/reportespma', reporteImpPmaRcdRoutes);
+app.use('/api/reportesrcd', reporteReporteRcdGenerador);
+app.use('/api/roles', roleRoutes);
+app.use('/api/pin', pinRoutes); // Rutas para buscar por PIN
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
