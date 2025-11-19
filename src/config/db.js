33f9292d450f1faad0eed26bb05dbed3f4eec228
Juan Lopez <juan.canchila@gmail.com>
2025-11-19 16:42:03 +0000

    new ip

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('epacartagena_escombros_1', 'epacartagena_bd_5', '97lXl&j58', {
-  host: '172.31.11.185:3306',
+  host: '34.234.151.198:8443',
   dialect: 'mariadb',
   logging: false,
   define: {
