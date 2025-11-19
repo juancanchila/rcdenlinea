e1967e9da98a5e58500f6462a528483247bf26d4
Juan Lopez <juan.canchila@gmail.com>
2025-11-19 16:49:29 +0000

    update con

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,22 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('epacartagena_escombros_1', 'epacartagena_bd_5', '97lXl&j58', {
-  host: ' 172.31.11.185:3306',
-  dialect: 'mariadb',
-  logging: false,
-  define: {
-    timestamps: false,
-    freezeTableName: true,
-  },
-});
+const sequelize = new Sequelize(
+  'epacartagena_escombros_1',      // BD
+  'epacartagena_bd_5',             // Usuario
+  '97lXl&j58',                     // Clave
+  {
+    host: '172.31.11.185',         // HOST LIMPIO
+    port: 3306,                    // AGREGA EL PUERTO (normal de MariaDB)
+    dialect: 'mariadb',
+    logging: false,
+    dialectOptions: {
+      timezone: 'Etc/GMT+5',       // opcional pero recomendado
+    },
+    define: {
+      timestamps: false,
+      freezeTableName: true,
+    },
+  }
+);
 
 module.exports = sequelize;
