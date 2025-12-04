ca6f43ac2dd7440f425fb02a11f264bc23027b0a
Juan Lopez <juan.canchila@gmail.com>
2025-12-04 12:33:55 -0500

    exportController

--- src/routes/exportRoutes.js
+++ src/routes/exportRoutes.js
@@ -1,7 +1,7 @@
 // routes/exportRoutes.js
 const express = require('express');
 const router = express.Router();
-const { exportAllPinsCsv } = require('../controllers/exportAllController');
+const { exportAllPinsCsv } = require('../controllers/exportController');
 
 router.get('/export/all', exportAllPinsCsv);
 
